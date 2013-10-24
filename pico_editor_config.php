cc2cfafda5f659da5b882537c5e07edf81ac066c
Roman Shvets <pomaxa@gmail.com>
2013-10-24 13:19:06 +0300

    remove default password

--- pico_editor_config.php
+++ pico_editor_config.php
@@ -6,6 +6,6 @@ global $pico_editor_password;
  * This should be a sha1 hash of your password.
  * Use a tool like http://www.sha1-online.com to generate.
  */
-$pico_editor_password = "c53255317bb11707d0f614696b3ce6f221d0e2f2";
+$pico_editor_password = "";
 
 ?>
